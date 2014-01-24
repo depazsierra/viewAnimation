841a69c334fec01546b60fcf626615e5760ee7b7
Depa <diegodepazsierra@gmail.com>
2014-01-24 11:52:03 -0300

    Init project

---
+++ viewAnimation/viewAnimation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  viewAnimation
+//
+//  Created by Diego de Paz Sierra on 1/24/14.
+//  Copyright (c) 2014 Depa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ viewAnimation/viewAnimation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  viewAnimation
+//
+//  Created by Diego de Paz Sierra on 1/24/14.
+//  Copyright (c) 2014 Depa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
